109a2bce552053d19f28c92f05340936fa189e89
brai83f <58195361+brai83f@users.noreply.github.com>
2019-11-25 21:59:08 -0500

    Plate format checker and date checker optimized

--- demo2.py
+++ demo2.py
@@ -8,89 +8,101 @@ a = True
 #    three letters -> [a-zA-Z]{3} followed by four numbers -> [0-9]{3}
 # Thats the two patterns of car plates allowed in Ecuador
 def plate_checker(raw_plate):
-    plate_format_a = compile('[a-zA-Z]{3}[0-9]{3}')
-    plate_format_b = compile('[a-zA-Z]{3}[0-9]{4}')
+    plate_format_a = compile('^[a-zA-Z]{3}[0-9]{3}$')
+    plate_format_b = compile('^[a-zA-Z]{3}[0-9]{4}$')
     print(raw_plate)
 
     for plate in raw_plate:
         if plate_format_a.match(plate) is not None or plate_format_b.match(plate) is not None:
-            print "Correct plate"
-            get_last_digit(raw_plate)
+            print "Correct plate-format"
+            return get_last_digit(raw_plate)
         else:
-            print "Incorrect plate"
-            a = False
-            break
+            print "Incorrect plate-format!!! Try again"
+            main()
 
 # Function to get the last digit of the already checked car-plate
 # Input must be the correct plate
 #    Returns the last digit of the car plate
 def get_last_digit(correct_plate):
     plate_str = str(correct_plate)
-    last_digit = plate_str[-3]
-    print last_digit
+    last_digit = int(plate_str[-3])
     return last_digit
 
 # Function with the pattern the input-year must match to be valid
 # Input must be a list containing the year
 #    Returns the valid year
-def year_checker(year):
-    year_format = compile('[[0-9]{4}')
+def year_format_checker(year):
+    year_format = compile('^[0-9]{4}$')
     print year
     for y in year:
         if year_format.match(y) is not None:
-            print "Correct year"
+            print "Correct year-format"
             return year
-            a = False
         else:
-            print "Incorrect year"
-            a = False
-            break
+            print "Incorrect year-format!!! Try it like \'yyyy\'"
+            return 0
 
 # Function with the pattern the input-month must match to be valid
 # Input must be a list containing the month
 #    Returns the valid month
-def month_checker(month):
-    month_format_a = compile('[[0-9]{2}')
-    month_format_b = compile('[[0-9]{1}')
+def month_format_checker(month):
+    month_format_a = compile('^[0-9]{2}$')
+    month_format_b = compile('^[0-9]{1}$')
     print month
     for m in month:
         if month_format_a.match(m) is not None or month_format_b.match(m) is not None:
-            print "Correct month"
+            print "Correct month-format"
             return month
-            a = False
         else:
-            print "Incorrect month"
-            a = False
-            break
+            print "Incorrect month-format!!! Try it like \'mm\'"
+            return 0
 
 # Function with the pattern the input-day must match to be valid
 # Input must be a list containing the day
 #    Returns the valid day
-def day_checker(day):
-    day_format_a = compile('[[0-9]{2}')
-    day_format_b = compile('[[0-9]{1}')
+def day_format_checker(day):
+    day_format_a = compile('^[0-9]{2}$')
+    day_format_b = compile('^[0-9]{1}$')
     print day
     for d in day:
         if day_format_a.match(d) is not None or day_format_b.match(d) is not None:
-            print "Correct day"
+            print "Correct day-format"
             return day
-            a = False
         else:
-            print "Incorrect day"
-            a = False
-            break
+            print "Incorrect day-format!!! Try it like \'dd\'"
+            return 0
 
 # Function to get the name of the day corresponding to the date
 # Input must be lists containing the year, month and day
 #    Returns the name of the day as a string
 def get_dayname(y,m,d):
     year = int(y[0])
+    print year
     month = int(m[0])
+    print month
     day = int(d[0])
+    print day
+    if year < 1900 or year > 3000 or month > 12 or month < 1 or day < 1 or day > 31:
+        print "Out of range date!!! Please try a valid date"
+        input_date()
     x = datetime.datetime(year, month, day)
     dayofweek = x.strftime("%A")
     print dayofweek
     return dayofweek
+
+def input_date():
+    input_year = [raw_input("Enter year \'yyyy\': ")]
+    while year_format_checker(input_year) == 0:
+        input_year = [raw_input("Enter year \'yyyy\': ")]
+
+    input_month = [raw_input("Enter month \'mm\': ")]
+    while month_format_checker(input_month) == 0:
+        input_month = [raw_input("Enter month \'mm\': ")]
+
+    input_day = [raw_input("Enter day \'dd\': ")]
+    while day_format_checker(input_day) == 0:
+        input_day = [raw_input("Enter day \'dd\': ")]
+    return input_year, input_month, input_day
 
 def check_time(raw_time):
     time_format = compile('[[0-9]{2}:[0-9]{2}')
@@ -117,20 +129,35 @@ def get_hour_n_minute(correct_time):
         print "Incorrect time"
         a = False
 
+def day_n_number(day_name):
+    weekdays = {
+        1: "Monday",
+        2: "Monday",
+        3: "Tuesday",
+        4: "Wednesday",
+        6: "Wednesday",
+        7: "Thursday",
+        8: "Thursday",
+        9: "Friday",
+        0: "Friday",
+        }
+    return weekdays.get(day_name, "There's not Pico y Placa running this day!!!")
+
 def main():
-#while a == True:
+
     print "Welcome to \"Pico y Placa\" checker program"
     input_plate = [raw_input("Enter car-plates: ")]
-    plate_checker(input_plate)
+    car_plate = plate_checker(input_plate)
+    print car_plate
 
-    print "Enter the date you want to check in order yyyy, mm, dd"
-    input_year = [raw_input("Enter year: ")]
-    year_checker(input_year)
-    input_month = [raw_input("Enter month: ")]
-    month_checker(input_month)
-    input_day = [raw_input("Enter day: ")]
-    day_checker(input_day)
-    get_dayname(input_year,input_month,input_day)
+    print "Enter the date you want to check in this order -> yyyy, mm, dd"
+    date = input_date()
+    print date
+    print date[0]
+    print date[1]
+    print date[2]
+
+    get_dayname(date[0],date[1],date[2])
 
     print "Enter the time you want to check in 24Hour format hh:mm"
     input_time = [raw_input("Enter time: ")]
